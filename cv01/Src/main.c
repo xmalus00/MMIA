ba4f2c8298d0a7a7ecb6989bb7d13bcf209c6bc0
xmalus00 <mimalu@seznam.cz>
2020-02-05 15:12:17 +0100

    cv1 v2

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -11,7 +11,21 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+#include "stm32f0xx.h"
+
 int main(void)
 {
-	for(;;);
+	RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;                   //zapnuti hodin
+	GPIOA -> MODER |= GPIO_MODER_MODER5_0;                 //
+
+	//  GPIOA->BSRR = (1<<5); // set
+	//  GPIOA->BRR = (1<<5); // reset
+	//GPIOA->ODR ^= (1<<5); // toggle
+
+
+	for(;;)
+		{
+		  GPIOA->ODR ^= (1<<5); // toggle
+		  for (volatile uint32_t i = 0; i < 100000; i++) {}
+		};
 }
