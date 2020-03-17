d6c3cae904bba3e77832c685742722bd8c9c4964
xmalus00 <xmalus00@vutbr.cz>
2020-03-17 18:51:53 +0100

    v5

--- cv06/MATLAB_lookup_table.m
+++ cv06/MATLAB_lookup_table.m
@@ -6,22 +6,25 @@ reading = csvread('ntc.csv');
 AD = reading(:,2);
 temperature = reading(:,1);
 
-ADval = 1023.*(AD./(AD+10))
+ADval = 1023.*(AD./(AD+10));
 
-plot(ADval,temperature);
-p=polyfit(ADval,temperature,10);
+figure
+plot(ADval,temperature,'o');
+hold on
+
+ p=polyfit(ADval,temperature,10);
 
 ad2=0:1023;
 t2 = round(polyval(p,ad2),1);
 
-hold on
+ hold on
 plot(ad2,t2,'r');
-
-hold on
-xlabel('ADC value') 
-ylabel('Temperature [°C]') 
-
-dlmwrite('data.dlm', t2*10,',');
+% 
+% hold on
+% xlabel('ADC value') 
+% ylabel('Temperature [°C]') 
+% 
+% dlmwrite('data.dlm', t2*10,',');
 
 
 
